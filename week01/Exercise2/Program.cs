10968fb7ff6f3b01e59ae85896af8482c77cc87a
Caleb <caleb.aves@gmail.com>
2026-07-08 07:20:24 -0300

    I did the second exercise of the first week. I added the lines to get the grade percentage from the user and then show the grade in the terminal.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage?");
+        string gradeInput = Console.ReadLine();
+        int gradePercentage = int.Parse(gradeInput);
+
+        if (gradePercentage >= 90)
+        {
+            Console.WriteLine("Congratulations, you get an 'A' grade.");
+        }
+        else if (gradePercentage >= 80)
+        {
+            Console.WriteLine("Congratulacions, you get a 'B' grade.");
+        }
+        else if (gradePercentage >= 70)
+        {
+            Console.WriteLine("Congratulations, you get a 'C' grade.");
+        }
+        else if (gradePercentage >= 60)
+        {
+            Console.WriteLine("You get a 'D' grade.");
+        }
+        else
+        {
+            Console.WriteLine("Sorry, you get an 'F' grade.");
+        }
     }
 }
