54f067146abaf2c41b125d974acf2da920422152
Caleb <caleb.aves@gmail.com>
2026-07-09 22:34:55 -0300

    I made the game of 'Guess my number'. I used the random class to generate a random number between 1 and 100, and then with a while loop and If sentences we can try to guess the number.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Console.WriteLine("Welcome to 'Guess my number game!'");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 100);
+        int i = 1;
+
+        while (i == 1)
+        {
+            Console.Write("Guess my number! ");
+            string guessString = Console.ReadLine();
+            int guess = int.Parse(guessString);
+
+            if (guess > number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                i = 0;
+            }
+        }
     }
 }
